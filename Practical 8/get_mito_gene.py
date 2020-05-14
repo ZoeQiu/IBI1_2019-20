445c44e05e5c8b53ce79155161d5a060633c21e7
ZoeQiu <61381390+ZoeQiu@users.noreply.github.com>
2020-05-14 23:48:21 +0800

    Update get_mito_gene.py

--- Practical 8/get_mito_gene.py
+++ Practical 8/get_mito_gene.py
@@ -2,12 +2,12 @@
 import re
 import os
 os.chdir("C:\\Users\\hh\\Desktop")
-a=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
+#open the Saccharomyces_cerevisiae file
+file=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
 new_file=open('mito_gene.fa',"w")
 
 #find all the mito genes by function 'findall'
-f=a.read()
-seq_mito=re.findall(r'(>.*?:Mito:[\d\D]+?gene:.*\n[\d\D]+?)>',f)
+seq_mito=re.findall(r'(>.*?:Mito:[\d\D]+?gene:.*\n[\d\D]+?)>',file.read())
 #creat a new file
 newfile=open('mito_gene.fa',"w")
 l=[]
@@ -17,11 +17,12 @@ for gene in seq_mito:
     simplified_seq_mito=re.sub(r'>.*?(gene:.*? ).*?]',r'\1',gene)
 #replace the '\n' and '>'
     final=simplified_seq_mito.replace('\n','')
-    length=len(final)-1
+    length=len(final)-11
     e=">"+'gene length:'+str(length)+' '+final+"\n"
     l.append(e)
     newfile.write(e)
-#close the newfile
+#close the file
+file.close()
 newfile.close()
 #print the final result
 print(open('mito_gene.fa').read())
