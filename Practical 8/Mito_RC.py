b64391fc8440d2b142e43d32c6b9634cb0ba1646
ZoeQiu <61381390+ZoeQiu@users.noreply.github.com>
2020-05-14 23:25:28 +0800

    Update Mito_RC.py

--- Practical 8/Mito_RC.py
+++ Practical 8/Mito_RC.py
@@ -1,3 +1,4 @@
+
 #import model and open file
 import re
 import os
@@ -6,7 +7,22 @@ a=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
 new_file=open('mito_gene.fa',"w")
 
 #find all the mito genes by function 'findall'
-f=a.read()
-seq_mito=re.findall(r'(>.*?:Mito:[\d\D]+?gene:.*\n[\d\D]+?)>',f)
+
+seq_mito=re.findall(r'(>.*?:Mito:[\d\D]+?gene:.*\n[\d\D]+?)>',a.read)
 #creat a new file
 newfile=open('mito_gene.fa',"w")
+l=[]
+#simplify the unnecessary part
+for gene in seq_mito:
+
+    simplified_seq_mito=re.sub(r'>.*?(gene:.*? ).*?]',r'\1',gene)
+#replace the '\n' and '>'
+    final=simplified_seq_mito.replace('\n','')
+    length=len(final)-11
+    e=">"+'gene length:'+str(length)+' '+final+"\n"
+    l.append(e)
+    newfile.write(e)
+#close the newfile
+newfile.close()
+#print the final result
+print(open('mito_gene.fa').read())
