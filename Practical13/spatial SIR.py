379e73888bfc4718d7ac5fcac2bab681566071a5
ZoeQiu <61381390+ZoeQiu@users.noreply.github.com>
2020-05-15 08:02:18 +0800

    Create spatial SIR.py

---
+++ Practical13/spatial SIR.py
@@ -0,0 +1,52 @@
+#import the necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+#make array of all susceptible population
+population=np.zeros((100,100))
+#Randomly decide the location of outbreak
+outbreak=np.random.choice(range(100),2)
+population [ outbreak [0] , outbreak [ 1 ] ] = 1
+#plot the initial infected individual
+plt.figure(figsize=(6,4),dpi=150)
+plt.imshow(population,cmap='viridis',interpolation='nearest')
+#Set the parameters
+beta=0.3
+gamma=0.05
+
+for t in range(100):
+    # find infected points
+    infectedIndex = np.where(population==1)
+    # loop through all infected points
+    for i in range(len(infectedIndex[0])):
+        # get x, y coordinates for each point
+        x = infectedIndex[0][i]
+        y = infectedIndex[1][i]
+        population[x,y]=np.random.choice(range(3),1,p=[0,gamma,1-gamma])[0]#People who recover from infections are defined as' 2 '
+        # infect each neighbour with probability beta
+        # infect all 8 neighbours (this is a bit finicky, is there a better way?):
+        for xNeighbour in range(x-1,x+2):
+            for yNeighbour in range(y-1,y+2):
+                # don't infect yourself! (Is this strictly necessary?)
+                if (xNeighbour,yNeighbour) != (x,y):
+                    # make sure I don't fall off an edge
+                    if xNeighbour != -1 and yNeighbour != -1 and xNeighbour!=100 and yNeighbour!=100:
+                        # only infect neighbours that are not already infected!
+                        if population[xNeighbour,yNeighbour]==0:
+                            population[xNeighbour,yNeighbour]=np.random.choice(range(2),1,p=[1-beta,beta])[0]
+                    
+            
+ #draw four plot
+    if t == 0:
+        plt.figure(figsize=(6,4),dpi=150)
+        plt.imshow(population,cmap='viridis',interpolation='nearest')
+    elif t == 9:
+        plt.figure(figsize=(6,4),dpi=150)
+        plt.imshow(population,cmap='viridis',interpolation='nearest')
+    elif t == 49:
+        plt.figure(figsize=(6,4),dpi=150)
+        plt.imshow(population,cmap='viridis',interpolation='nearest')
+    elif t == 99:
+        plt.figure(figsize=(6,4),dpi=150)
+        plt.imshow(population,cmap='viridis',interpolation='nearest')
+                            
+    
